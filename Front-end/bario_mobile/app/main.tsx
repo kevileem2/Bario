fe3c7204d5e305e05375baa15ce139901adab4c9
kevileem2 <kevileem2@student.arteveldehs.be>
2019-10-07 10:45:37 +0200

    added bario icon & updated config files

---
+++ Front-end/bario_mobile/app/main.tsx
@@ -0,0 +1,17 @@
+//React imports
+import React from "react"
+import { View, Text } from "react-native"
+
+/**
+ * @class Main
+ * Root of App, this is used to not directly edit the 'App.js' file in the root folder
+ */
+const Main: React.FC = () => {
+  return (
+    <View>
+      <Text>Test!</Text>
+    </View>
+  )
+}
+
+export default Main
