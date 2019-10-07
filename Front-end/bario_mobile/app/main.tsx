fe587d844270f60d991ef4e1c218af5135b1585e
kevileem2 <kevileem2@student.arteveldehs.be>
2019-10-07 13:59:55 +0200

    prettier file

--- Front-end/bario_mobile/app/main.tsx
+++ Front-end/bario_mobile/app/main.tsx
@@ -1,19 +1,13 @@
-//React imports
-import React from 'react'
-import { View} from "react-native"
-
+// React imports
+import React from "react"
+import { View } from "react-native"
 
 /**
  * @class Main
  * Root of App, this is used to not directly edit the 'App.js' file in the root folder
  */
 const Main: React.FC = () => {
-
-  return (
-    <View>
-
-    </View>
-  )
+  return <View />
 }
 
 export default Main
