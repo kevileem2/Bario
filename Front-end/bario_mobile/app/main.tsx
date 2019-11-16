c0ba21293ee9594436c60a39235207ff942b0f2a
hektormisplon <hektor.misplon@gmail.com>
2019-11-16 22:41:00 +0100

    custom fonts & async assets load

--- Front-end/bario_mobile/app/main.tsx
+++ Front-end/bario_mobile/app/main.tsx
@@ -1,15 +1,46 @@
-//React imports
-import React from "react"
-import BackgroundImage from "./shared/backgroundImage/BackgroundImage"
+// React imports
+import React, { useState } from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 
 /**
  * @class Main
  * Root of App, this is used to not directly edit the 'App.js' file in the root folder
  */
+
 export default () => {
-  return (
-    <BackgroundImage
-      source={require("./assets/belfort-background.jpg")}
-    ></BackgroundImage>
-  )
-}
+  const [isLoading, setLoading] = useState(true);
+  const _loadResourcesAsync = async () => {
+    await Promise.all([
+      Font.loadAsync({
+        Inter: require("../assets/fonts/Inter-Regular.ttf"),
+        InterBold: require("../assets/fonts/Inter-Bold.ttf")
+      })
+    ]);
+  };
+
+  if (isLoading) {
+    return (
+      <AppLoading
+        startAsync={_loadResourcesAsync}
+        onFinish={() => setLoading(false)}
+        onError={console.warn}
+      />
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff"
+  }
+});
