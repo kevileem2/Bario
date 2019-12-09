2ff57a11f39acf914310db9d6352125047cd6204
Hektor Misplon <hektor.misplon@gmail.com>
2019-12-09 09:27:30 +0100

    social service mock data

--- Front-end/bario_mobile/app/screens/SocialService/SocialService.tsx
+++ Front-end/bario_mobile/app/screens/SocialService/SocialService.tsx
@@ -1,13 +1,38 @@
-import React from "react"
-import { Text } from "../../shared"
+import React, { useState, useEffect } from "react"
+import { View, TouchableOpacity, StyleSheet, FlatList, Platform, ActivityIndicator } from 'react-native'
 import { Container } from "./components"
-
-
+import json from './data.json'
+import { Title, Subtitle, Text } from '_theme/typography'
 
 export default ({ navigation }) => {
+  const [data, setData] = useState([])
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // let data = await (await fetch('./data')).json();
+        setData(json.data);
+      } catch (err) {
+        setError(true);
+      }
+      setLoading(false);
+    }
+    fetchData()
+  }, [])
+
   return (
     <Container>
-      <Text type="title">Social Services</Text>
+      <Title>Social Services</Title>
+      <Text>{json.current_page}</Text>
+      {isError && <Text>Something went wrong</Text>}
+      {isLoading ? <ActivityIndicator /> : (
+        <FlatList data={data} keyExtractor={item => item.id.toString()} renderItem={({ item }) => (
+          <View key={item.id}>
+            <Subtitle>{item.name}</Subtitle>
+          </View>
+        )} />
+      )}
     </Container>
   )
 }
