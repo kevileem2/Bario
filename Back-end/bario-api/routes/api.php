011e870aa9ddd099ed59cd6187f35840e74ba165
juan <juanmuno@student.arteveldehs.be>
2020-01-10 19:16:36 +0100

    routes changed

--- Back-end/bario-api/routes/api.php
+++ Back-end/bario-api/routes/api.php
@@ -16,47 +16,51 @@ use Illuminate\Http\Request;
     /**
      * Access Token Authentication
      */
+
     Route::post('/register', 'Api\AuthController@register' );
     Route::post('/login', 'Api\AuthController@login' );
 
-    // Middleware for social services
-    Route::get('/socialServices','API\SocialServicesController@index')->middleware('checkUser');
-    Route::post('/socialServices','API\SocialServicesController@store')->middleware('checkModerator');
-    Route::get('/socialServices/{id}','API\SocialServicesController@show')->middleware('checkUser');
-    Route::patch('/socialServices/update/{id}','API\SocialServicesController@update')->middleware('checkModerator');
-    Route::delete('/socialServices/delete/{id}','API\SocialServicesController@destroy')->middleware('checkModerator');
-    
-    // Middleware for categories
-    // Get all tags from a category
-    Route::get('/categories/{id}/tags','API\CategoriesController@categoryTags')->middleware('checkUser');
-    
-    Route::get('/categories','API\CategoriesController@index')->middleware('checkUser');
-    Route::post('/categories','API\CategoriesController@store')->middleware('checkModerator');
-    Route::get('/categories/{id}','API\CategoriesController@show')->middleware('checkUser');
-    Route::patch('/categories/update/{id}','API\CategoriesController@update')->middleware('checkModerator');
-    Route::delete('/categories/delete/{id}','API\CategoriesController@destroy')->middleware('checkModerator');
-    
-    // Middleware for events
-    // Get event creator 
-    Route::get('/events/{id}/creator','API\EventsController@getCreator')->middleware('checkUser');
-    Route::get('/events/{id}/category','API\EventsController@getCategory')->middleware('checkUser');
-    Route::get('/events/{id}/tags','API\EventsController@getTags')->middleware('checkUser');
 
-    Route::get('/events','API\EventsController@index')->middleware('checkUser');
-    Route::post('/events','API\EventsController@store')->middleware('checkModerator');
-    Route::get('/events/{id}','API\EventsController@show')->middleware('checkUser');
-    Route::patch('/events/update/{id}','API\EventsController@update')->middleware('checkModerator');
-    Route::delete('/events/delete/{id}','API\EventsController@destroy')->middleware('checkModerator');
-    
-    // Middleware for tags
-    Route::get('/tags','API\TagsController@index')->middleware('checkUser');
-    Route::get('/tags','API\TagsController@index')->middleware('checkUser');
-    Route::post('/tags','API\TagsController@store')->middleware('checkModerator');
-    Route::get('/tags/{id}','API\TagsController@show')->middleware('checkUser');
-    Route::patch('/tags/update/{id}','API\TagsController@update')->middleware('checkModerator');
-    Route::delete('/tags/delete/{id}','API\TagsController@destroy')->middleware('checkModerator');
+    /**
+     * User Routes
+     */
+
+    Route::group(['middleware' => ['auth:api','checkUser']], function() {
+
+        Route::get('/socialServices','API\SocialServicesController@index');
+        Route::get('/socialServices/{id}','API\SocialServicesController@show');
+        Route::get('/categories/{id}/tags','API\CategoriesController@categoryTags');
+        Route::get('/categories','API\CategoriesController@index');
+        Route::get('/categories/{id}','API\CategoriesController@show');
+        Route::get('/events','API\EventsController@index');
+        Route::get('/events/{id}/creator','API\EventsController@getCreator');
+        Route::get('/events/{id}/category','API\EventsController@getCategory');
+        Route::get('/events/{id}/tags','API\EventsController@getTags');
+        Route::get('/events/{id}','API\EventsController@show');
+        Route::get('/tags','API\TagsController@index');
+        Route::get('/tags','API\TagsController@index');
+        Route::get('/tags/{id}','API\TagsController@show');
 
 
-    Route::post('file/SocialServicesImage','FileManagementController@SocialServicesImageUpload')->middleware('checkModerator');
+    });
 
+    /**
+     * Admin routes
+     */
+    Route::group(['middleware' =>['auth:api','checkModerator']], function() {
 
+        Route::post('/socialServices','API\SocialServicesController@store');
+        Route::patch('/socialServices/update/{id}','API\SocialServicesController@update');
+        Route::delete('/socialServices/delete/{id}','API\SocialServicesController@destroy');
+        Route::post('/categories','API\CategoriesController@store');
+        Route::patch('/categories/update/{id}','API\CategoriesController@update');
+        Route::delete('/categories/delete/{id}','API\CategoriesController@destroy');
+        Route::post('/events','API\EventsController@store');
+        Route::patch('/events/update/{id}','API\EventsController@update');
+        Route::delete('/events/delete/{id}','API\EventsController@destroy');
+        Route::patch('/tags/update/{id}','API\TagsController@update');
+        Route::delete('/tags/delete/{id}','API\TagsController@destroy');
+        Route::post('file/SocialServicesImage','FileManagementController@SocialServicesImageUpload');
+        Route::post('/tags','API\TagsController@store');
+
+    });
