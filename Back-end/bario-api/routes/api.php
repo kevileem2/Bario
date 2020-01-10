00c4c3cbd3f3d5b1008585eafb90efce7084c8b0
Yoram <yoraplat@student.arteveldehs.be>
2020-01-10 15:46:42 +0100

    added api routes

--- Back-end/bario-api/routes/api.php
+++ Back-end/bario-api/routes/api.php
@@ -20,7 +20,6 @@ use Illuminate\Http\Request;
     Route::post('/login', 'Api\AuthController@login' );
 
     // Middleware for social services
-    // Route::apiResource('/socialServices','API\SocialServicesController')->middleware('checkAdmin');
     Route::get('/socialServices','API\SocialServicesController@index')->middleware('checkUser');
     Route::post('/socialServices','API\SocialServicesController@store')->middleware('checkModerator');
     Route::get('/socialServices/{id}','API\SocialServicesController@show')->middleware('checkUser');
@@ -28,7 +27,9 @@ use Illuminate\Http\Request;
     Route::delete('/socialServices/delete/{id}','API\SocialServicesController@destroy')->middleware('checkModerator');
     
     // Middleware for categories
-    // Route::apiResource('/categories','API\CategoriesController');
+    // Get all tags from a category
+    Route::get('/categories/{id}/tags','API\CategoriesController@categoryTags')->middleware('checkUser');
+    
     Route::get('/categories','API\CategoriesController@index')->middleware('checkUser');
     Route::post('/categories','API\CategoriesController@store')->middleware('checkModerator');
     Route::get('/categories/{id}','API\CategoriesController@show')->middleware('checkUser');
@@ -36,7 +37,11 @@ use Illuminate\Http\Request;
     Route::delete('/categories/delete/{id}','API\CategoriesController@destroy')->middleware('checkModerator');
     
     // Middleware for events
-    // Route::apiResource('/events','API\EventsController');
+    // Get event creator 
+    Route::get('/events/{id}/creator','API\EventsController@getCreator')->middleware('checkUser');
+    Route::get('/events/{id}/category','API\EventsController@getCategory')->middleware('checkUser');
+    Route::get('/events/{id}/tags','API\EventsController@getTags')->middleware('checkUser');
+
     Route::get('/events','API\EventsController@index')->middleware('checkUser');
     Route::post('/events','API\EventsController@store')->middleware('checkModerator');
     Route::get('/events/{id}','API\EventsController@show')->middleware('checkUser');
@@ -44,7 +49,7 @@ use Illuminate\Http\Request;
     Route::delete('/events/delete/{id}','API\EventsController@destroy')->middleware('checkModerator');
     
     // Middleware for tags
-    // Route::apiResource('/tags','API\TagsController');
+    Route::get('/tags','API\TagsController@index')->middleware('checkUser');
     Route::get('/tags','API\TagsController@index')->middleware('checkUser');
     Route::post('/tags','API\TagsController@store')->middleware('checkModerator');
     Route::get('/tags/{id}','API\TagsController@show')->middleware('checkUser');
