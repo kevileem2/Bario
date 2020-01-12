0bc2f6d197522ea2cd94316bad856df922b0dd4e
kevileem2 <kevileem2@student.arteveldehs.be>
2020-01-12 11:27:23 +0100

    add files to project repo

--- Front-end/bario_mobile/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ Front-end/bario_mobile/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp563d9ca10b44449ca1a0bb872048e688://192.168.0.236:19000";
 
 }
